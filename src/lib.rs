89dc3b8db101c797bb936700f367e4233440a722
ysc <skyholmes@163.com>
2023-08-28 19:29:50 +0800

    find crate lastest version

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 use anyhow::{anyhow, Result};
-use std::fs;
+use reqwest::blocking::Client;
+use serde::Deserialize;
+use std::{fs, sync::mpsc, thread};
 use toml::{Table, Value};
+
+const USER_AGENT:&str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36";
 
 #[derive(Debug)]
 pub struct Crate {
     pub name: String,
     pub version: String,
-    pub features: Vec<String>,
+    pub features: String,
 }
 
 impl Crate {
@@ -14,7 +18,7 @@ impl Crate {
         Self {
             name,
             version: String::new(),
-            features: Vec::new(),
+            features: String::new(),
         }
     }
 }
@@ -33,11 +37,13 @@ pub fn read_cargo(path: &str) -> Result<Vec<Crate>> {
                         c.version = version.clone();
                     }
                     if let Some(Value::Array(features)) = table.get("features") {
+                        let mut list = Vec::new();
                         for v in features {
                             if let Value::String(f) = v {
-                                c.features.push(f.clone());
+                                list.push(f.clone());
                             }
                         }
+                        c.features = list.join(",");
                     }
                 }
                 _ => {}
@@ -60,4 +66,74 @@ fn test_read_cargo() {
     }
 }
 
+pub fn filter_lastest_crate(list: Vec<Crate>) -> Vec<Crate> {
+    let (tx, rx) = mpsc::sync_channel(10);
+    for c in list {
+        let tx = tx.clone();
+        thread::spawn(move || {
+            let ver = c.version.clone();
+            match query_crate_lastest_version(c) {
+                Ok(lastest) => {
+                    if lastest.version != ver {
+                        tx.send(lastest).expect("send crate error");
+                    }
+                }
+                Err(e) => println!("query crate lastest version error: {e:?}"),
+            }
+        });
+    }
+    drop(tx);
+    let mut res = Vec::new();
+    for c in rx {
+        res.push(c);
+    }
+    res
+}
 
+#[test]
+fn test_filter_lastest_crate() {
+    let list = vec![
+        Crate::new_by_name("anyhow".into()),
+        Crate::new_by_name("tokio".into()),
+        Crate {
+            name: "serde".into(),
+            version: "1.0.188".into(),
+            features: "".into(),
+        },
+    ];
+    let list = filter_lastest_crate(list);
+    println!("list: {:#?}", list);
+}
+
+#[derive(Deserialize, Debug)]
+struct LastestResp {
+    #[serde(rename = "crate")]
+    crate_obj: CrateObj,
+}
+
+#[derive(Deserialize, Debug)]
+struct CrateObj {
+    max_stable_version: String,
+}
+
+pub fn query_crate_lastest_version(c: Crate) -> Result<Crate> {
+    let resp: LastestResp = Client::builder()
+        .user_agent(USER_AGENT)
+        .build()?
+        .get(format!("https://crates.io/api/v1/crates/{}", c.name))
+        .send()?
+        .json()?;
+    Ok(Crate {
+        name: c.name,
+        version: resp.crate_obj.max_stable_version,
+        features: c.features,
+    })
+}
+
+#[test]
+fn test_query_crate_lastest_version() {
+    match query_crate_lastest_version(Crate::new_by_name("tokio".into())) {
+        Ok(c) => println!("crate: {:?}", c),
+        Err(e) => println!("err: {e:?}"),
+    }
+}
